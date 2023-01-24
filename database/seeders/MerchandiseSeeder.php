10b421c4557e7d5cdfcb34bb6c5630c954f782c7
012shr <117974639+012shr@users.noreply.github.com>
2023-01-24 20:29:38 +0700

    Update MerchandiseSeeder.php

--- database/seeders/MerchandiseSeeder.php
+++ database/seeders/MerchandiseSeeder.php
@@ -19,7 +19,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 50000,
             'detail' => "This is a huggable komodo plush. DISCLAIMER: real life komodos are not huggable, unlike this plush!",
             'stock' => 500,
-            'image' => ""
+            'image' => "https://safariwonders.com/wp-content/uploads/2016/08/50.-komodo-32_dsc7679-1471577190-320x320.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
