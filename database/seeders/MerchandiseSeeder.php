d055fac6cbf013070693f8ca1a27ea5ce1fdcfbf
012shr <117974639+012shr@users.noreply.github.com>
2023-01-24 20:38:46 +0700

    Update MerchandiseSeeder.php

--- database/seeders/MerchandiseSeeder.php
+++ database/seeders/MerchandiseSeeder.php
@@ -23,12 +23,12 @@ class MerchandiseSeeder extends Seeder
         ]);
 
         DB:: table('merchandises')->insert([
-            'id' => 1,
-            'name' => "Komodo Plush",
-            'price' => 50000,
-            'detail' => "This is a huggable komodo plush. DISCLAIMER: real life komodos are not huggable, unlike this plush!",
-            'stock' => 500,
-            'image' => ""
+            'id' => 2,
+            'name' => "Komodo T-Shirt",
+            'price' => 60000,
+            'detail' => "Cotton Combed 20s, Size - Unisex: L ( LD 50, P 71 )",
+            'stock' => 300,
+            'image' => "https://id-live-01.slatic.net/p/9fdc7f8a0cb52b59966486b711a954bc.jpg"
         ]);
     }
 }
