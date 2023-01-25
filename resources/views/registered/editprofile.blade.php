2bebbe3cfea55b9585687d61cb51cceec9b519e0
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2023-01-25 23:33:39 +0700

    update editprofile.blade

--- resources/views/registered/editprofile.blade.php
+++ resources/views/registered/editprofile.blade.php
@@ -5,80 +5,62 @@
 @endsection
 
 @section('content')
-    <div class="container rounded bg-white mt-5 mb-5">
-        <div class="row">
-            <div class="col-md-5 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+    <div class="container rounded bg-white mt-5 mb-5 px-5">
+        <div class="d-flex flex-row">
+            <div class="d-flex flex-column text-center p-3 py-5 gap-4 border-right col-md-5">
+                <div style="width: min-content">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                            <div class="profile-picture">
+                                <img src="path/to/default-picture.jpg" alt="Profile Picture"
+                                     class="img-fluid rounded-circle">
 
-
-
-
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 text-center">
-                                <div class="profile-picture">
-                                    <img src="path/to/default-picture.jpg" alt="Profile Picture"
-                                        class="img-fluid rounded-circle">
-                                    {{-- <form>
-                                        <div class="form-group">
-                                            <label for="profile-picture-input" class="btn btn-primary btn-sm">
-                                                Update Picture
-                                            </label>
-                                            <input type="file" class="form-control-file d-none"
-                                                id="profile-picture-input">
-                                        </div>
-                                    </form> --}}
-                                </div>
+                                {{-- <form> --}}
+                                    {{-- <div class=""> --}}
+                                        {{-- <label for="" class="">
+                                            Update Picture
+                                        </label> --}}
+                                        {{-- <input type="file" class="form-control-file d-none"
+                                            id=""> --}}
+                                    {{-- </div> --}}
+                                {{-- </form> --}}
                             </div>
                         </div>
                     </div>
+                </div>
 
 
-
-
-
-
-
-
-
-
-
-
-                    <img class="mt-5" style="  width: 250px;
+                <img style="  width: 250px;
                     height: 250px; border-radius: 10px;"
-                        src="https://i.pinimg.com/564x/18/b5/b5/18b5b599bb873285bd4def283c0d3c09.jpg">
+                     src="https://i.pinimg.com/564x/18/b5/b5/18b5b599bb873285bd4def283c0d3c09.jpg">
 
-                        <form style="margin-top: 8%; padding: 0;">
-                            <div class="form-group" >
+                <form class="m-0 p-0" style="width: min-content">
+                    <label for="profile-picture-input" class="btn btn-primary btn-sm m-0">
+                        Update Picture
+                    </label>
 
-                                <label for="profile-picture-input" class="btn btn-primary btn-sm" >
-                                    Update Picture
-                                </label>
-
-                                <input type="file" class="form-control-file d-none"
-                                    id="profile-picture-input">
-                            </div>
-                        </form>
+                    <input type="file" class="form-control-file d-none"
+                           id="profile-picture-input">
+                </form>
 
 
-                    <div class="col-md-8" style="margin: 3%;">
-                        <div class="form-group-1">
+                <div class="m-0 p-0 col-md-10" style="margin: 3%;">
+                    <div class="form-group-1">
 
-                            <label class="labels" for="update-name" style="float: left;">New Username
-                            </label>
+                        <label class="labels" for="update-name" style="float: left;">New Username
+                        </label>
 
-                            <input type="text" class="form-control" placeholder="Diva Angelika" value=""
-                                id="update-name">
-                        </div>
+                        <input type="text" class="form-control" placeholder="Diva Angelika" value=""
+                               id="update-name">
+                    </div>
 
-                        <div class="form-group-2" style="margin-top: 5%">
+                    <div class="form-group-2" style="margin-top: 5%">
 
-                            <label class="labels" for="update-name" style="float: left;">Current Email
-                            </label>
-                            <br>
-                            <input type="text" class="form-control" placeholder="diva@mail.com" value=""
-                                id="update-name" readonly>
-                        </div>
+                        <label class="labels" for="update-name" style="float: left;">Current Email
+                        </label>
+                        <br>
+                        <input type="text" class="form-control" placeholder="diva@mail.com" value=""
+                               id="update-name" readonly>
                     </div>
                 </div>
             </div>
@@ -117,7 +99,7 @@
                             <button class="btn btn-primary profile-button-save" style="float: left;" type="button">
                                 <a href="/profile" style="text-decoration: none; color: white">
                                     Save
-                                Profile
+                                    Profile
                                 </a>
                             </button>
                         </div>
