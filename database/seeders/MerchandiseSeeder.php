98dfdbd1ef7c2012fe0b0bb1e117559aef0ebf38
rhenaldy <rhenaldy.class@gmail.com>
2023-01-25 12:56:22 +0700

    merchandise images, transferred to local public storage

--- database/seeders/MerchandiseSeeder.php
+++ database/seeders/MerchandiseSeeder.php
@@ -20,7 +20,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 50000,
             'detail' => "This is a huggable komodo plush. DISCLAIMER: real life komodos are not huggable, unlike this plush!",
             'stock' => 500,
-            'image' => "https://safariwonders.com/wp-content/uploads/2016/08/50.-komodo-32_dsc7679-1471577190-320x320.jpg"
+            'image' => "merch/komodo-plush.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -29,7 +29,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 60000,
             'detail' => "Cotton Combed 20s, Size - Unisex: L ( LD 50, P 71 )",
             'stock' => 300,
-            'image' => "https://id-live-01.slatic.net/p/9fdc7f8a0cb52b59966486b711a954bc.jpg"
+            'image' => "merch/komodo-tshirt.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -38,7 +38,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 30000,
             'detail' => "Super durable and water-resistant",
             'stock' => 200,
-            'image' => "https://ih1.redbubble.net/image.1669427998.4622/st,small,507x507-pad,600x600,f8f8f8.jpg"
+            'image' => "merch/komodo-sticker.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -47,7 +47,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 30000,
             'detail' => "Super durable and water-resistant",
             'stock' => 400,
-            'image' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSasEYnv8hLLMggAleOplaV9GDlVEV1i3bT8a-6bhCCWDiTHSGEcyu27rVw8UCiW-lf38o&usqp=CAU"
+            'image' => "merch/i-love-komodo.jpeg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -56,7 +56,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 20000,
             'detail' => "Unique artwork, Digitally printed cards on heavyweight stock",
             'stock' => 150,
-            'image' => "https://ih1.redbubble.net/image.651703652.5608/paperpc,750x,w,f8f8f8-pad,1000x1000,f8f8f8.u2.jpg"
+            'image' => "merch/komodo-post-card.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -65,7 +65,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 60000,
             'detail' => "Holds 11 oz. (325 ml), Dishwasher-safe ceramic",
             'stock' => 300,
-            'image' => "https://rlv.zcache.com/realistic_komodo_dragon_monitor_lizard_coffee_mug-r2f0eeaf6be724aaab22a0aba689e6d7f_x7jg5_8byvr_736.webp"
+            'image' => "merch/komodo-mug.png"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -74,7 +74,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 50000,
             'detail' => "Full-color double-sided design printed, Machine washable",
             'stock' => 200,
-            'image' => "https://res.cloudinary.com/teepublic/image/private/s--4smZA6fs--/c_crop,x_10,y_10/c_fit,h_788/c_crop,g_north_west,h_1051,w_1051,x_-214,y_-129/l_upload:v1507037314:production:blanks:gbajnunp66ec7xftnpq1/fl_layer_apply,g_north_west,x_-319,y_-228/b_rgb:ffffff/c_limit,f_auto,h_630,q_90,w_630/v1668282209/production/designs/36467378_0.jpg"
+            'image' => "merch/komodo-pillow.png"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -83,7 +83,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 70000,
             'detail' => "Thickness 1/16 inch (1.6mm), Available for iPhone 7 Plus / 8 Plus",
             'stock' => 300,
-            'image' => "https://m.media-amazon.com/images/I/41FZFxUwsdS._AC_SY580_.jpg"
+            'image' => "merch/komodo-phone-case.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -92,7 +92,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 80000,
             'detail' => "Made from 100% polyester woven fabric with high-quality metal grommets",
             'stock' => 500,
-            'image' => "https://ih1.redbubble.net/image.198874179.9335/drawstring_bag,x750-pad,750x1000,f8f8f8.jpg"
+            'image' => "merch/komodo-drawstring-bag.jpg"
         ]);
 
         DB:: table('merchandises')->insert([
@@ -101,7 +101,7 @@ class MerchandiseSeeder extends Seeder
             'price' => 30000,
             'detail' => "120 pages, Cover 350gsm, paper stock 90gsm",
             'stock' => 200,
-            'image' => "https://ih1.redbubble.net/image.850541430.2963/sn,x1000-pad,1000x1000,f8f8f8.u3.jpg"
+            'image' => "merch/komodo-spiral-notebook.jpg"
         ]);
     }
 }
