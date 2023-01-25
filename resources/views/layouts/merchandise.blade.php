a57d2b8d67a6d3b41947dde688fa14d7cdd58238
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2023-01-25 13:48:53 +0700

    update merchandise

--- resources/views/layouts/merchandise.blade.php
+++ resources/views/layouts/merchandise.blade.php
@@ -7,10 +7,11 @@
 @section('content')
 
         <!-- Navigation-->
-        {{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                {{-- <a class="navbar-brand" href="#!">Start Bootstrap</a> --}}
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    {{-- <span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
@@ -24,7 +25,7 @@
                                 <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
                             </ul>
                         </li>
-                    </ul>
+                    </ul> --}}
                     <form class="d-flex">
                         <button class="btn btn-outline-dark" type="submit">
                             <i class="bi-cart-fill me-1"></i>
@@ -34,7 +35,8 @@
                     </form>
                 </div>
             </div>
-        </nav> --}}
+        </nav>
+
         <!-- Header-->
         {{-- <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
