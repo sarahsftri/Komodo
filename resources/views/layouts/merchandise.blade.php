c3ab9a9acddbd058d01ebc76e767035c4a3465b6
Diva Angelika Mulia <diva.angelika.mulia@gmail.com>
2023-01-25 14:36:44 +0700

    update merchandise.blade

--- resources/views/layouts/merchandise.blade.php
+++ resources/views/layouts/merchandise.blade.php
@@ -37,19 +37,17 @@
             </div>
         </nav>
 
-        <!-- Header-->
-        {{-- <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
-                </div>
-            </div>
-        </header> --}}
         <!-- Section-->
+
+
+
         <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
+                <div>
+                    <h1>Latest</h1>
+                </div>
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
@@ -170,8 +168,13 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="col mb-5">
+                        <div>
+                            <h1>TEST</h1>
+                        </div>
                         <div class="card h-100">
+
                             <!-- Product image-->
                             <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                             <!-- Product details-->
