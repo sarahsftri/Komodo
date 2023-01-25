aad9a62ae4ac2d1bf2fd0602abf228957808aef5
rhenaldy <rhenaldy.class@gmail.com>
2023-01-25 09:42:07 +0700

    middleware

--- app/Http/Middleware/Member.php
+++ app/Http/Middleware/Member.php
@@ -5,8 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cookie;
-use Illuminate\Support\Facades\Session;
 
 class Member
 {
