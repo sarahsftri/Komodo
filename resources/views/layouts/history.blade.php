cb5d181f06c4e80cc9f738dc2ca1217f40373e3b
rhenaldy <rhenaldy.class@gmail.com>
2023-01-25 16:01:27 +0700

    Update history.blade.php

--- resources/views/layouts/history.blade.php
+++ resources/views/layouts/history.blade.php
@@ -6,19 +6,59 @@
     <br>
     <div class="container" style = "background-color: #FFFFFF; border-radius: 25px;">
         <h2>Ticket</h2>
-        @foreach ($apa as $apas)
+        @foreach ($history as $hist)
+        @if($hist->type == 'Ticket')
         <div class="item-image">
             <img src="{{asset('#')}}">
         </div>
         <div class="item-detail mt-3">
             <h5>
-                {{$apa->apa->name}}
+                Ticket ID: {{$hist->ticket->id}}
             </h5>
 
-            <p>Quantity: {{$apa>quantity}}</p>
+            <p>Quantity: {{$hist->quantity}}</p>
 
-            <p>Total Price: IDR {{$apa = $apa->apa->apa * $apa->quantity}}</p>
+            <p>Total Price: IDR {{$hist->total_price}}</p>
         </div>
+        @endif
+        @endforeach
+    </div>
+    <div class="container" style = "background-color: #FFFFFF; border-radius: 25px;">
+        <h2>Merchandise</h2>
+        @foreach ($history as $hist)
+        @if($hist->type == 'Merchandise')
+        <div class="item-image">
+            <img src="{{asset('#')}}">
+        </div>
+        <div class="item-detail mt-3">
+            <h5>
+                Ticket ID: {{$hist->ticket->id}}
+            </h5>
+
+            <p>Quantity: {{$hist->quantity}}</p>
+
+            <p>Total Price: IDR {{$hist->total_price}}</p>
+        </div>
+        @endif
+        @endforeach
+    </div>
+    <div class="container" style = "background-color: #FFFFFF; border-radius: 25px;">
+        <h2>Donation</h2>
+        @foreach ($history as $hist)
+        @if($hist->type == 'Donation')
+        <div class="item-image">
+            <img src="{{asset('#')}}">
+        </div>
+        <div class="item-detail mt-3">
+            <h5>
+                Ticket ID: {{$hist->ticket->id}}
+            </h5>
+
+            <p>Quantity: {{$hist->quantity}}</p>
+
+            <p>Total Price: IDR {{$hist->total_price}}</p>
+        </div>
+        @endif
         @endforeach
     </div>
 </body>
